9caeb54de49286cd33d04d3263ddcc1db5cfd77f
Henrik Renlund <henrikrenlund@gmail.com>
2015-04-28 08:58:03 +0200

    (small fixes)

--- R/data_man_create.r
+++ R/data_man_create.r
@@ -4,13 +4,15 @@
 #' @param container name of \code{data_man_container} object
 #' @param id.name identifier across data bases or a list of such
 #' @param na.where if entry \code{where} is \code{NA} in
+#' @param dplyr use dplyr for merging operations? (Default: \code{TRUE}.)
 #' \code{data_man_container}, this object is used in its place.
 #' @export
 
 data_man_create <- function(id,
                             container,
                             id.name,
-                            na.where = NULL
+                            na.where = NULL,
+                            dplyr = TRUE
 ){
     if(missing(id.name)) id.name <- "id"
     if(missing(container)){
@@ -24,7 +26,7 @@ data_man_create <- function(id,
                  error = function(e) stop("[data_man_create] can't find 'na.where'"))
         if(!identicalid) id.name[["NAW"]] <- id.name[[na.where]]
     }
-    DF <- data.frame(id)
+    DF <- if(dplyr) dplyr::data_frame(id) else data.frame(id)
     names(DF) <- the_id
 
     for(indx in seq_along(container)){ # indx <- 17
@@ -52,13 +54,23 @@ data_man_create <- function(id,
             }
             )
         }
-        loc.df <- data.frame(
-            tmp,
-            get(df)[[if(identicalid) the_id else id.name[df][[1]]]]
-        )
-        names(loc.df) <- c(name, the_id)
-        DF <- merge(DF, loc.df, by.x = the_id)
-        if(!is.null(X$label)) attr(DF[[name]], "label") <- X$label
+        if(dplyr){
+            loc.df <- dplyr::data_frame(
+                tmp,
+                get(df)[[if(identicalid) the_id else id.name[df][[1]]]]
+            )
+            names(loc.df) <- c(name, the_id)
+            DF <- dplyr::inner_join(DF, loc.df, by = the_id)
+            if(!is.null(X$label)) attr(DF[[name]], "label") <- X$label
+        } else{
+            loc.df <- data.frame(
+                tmp,
+                get(df)[[if(identicalid) the_id else id.name[df][[1]]]]
+            )
+            names(loc.df) <- c(name, the_id)
+            DF <- merge(DF, loc.df, by.x = the_id)
+            if(!is.null(X$label)) attr(DF[[name]], "label") <- X$label
+        }
     }
     DF
 }
--- R/data_man_get_recode.r
+++ R/data_man_get_recode.r
@@ -3,19 +3,22 @@
 #' @param container the 'data_man' container
 #' @param file 'file' argument for \code{Hmisc::latex} (default = "")
 #' @param lab.prefix prefix for labels for LaTeX tables (default = "tab:recode_") which concatenates with the variable name
+#' @param where LaTeX indicator of position for float (Default: "htb")
+#' @param clearpage how many floats before a LaTeX clearpage? Recycled if necessary
 #' @param ... arguments passed to \code{Hmisc::latex}.
 #' @author Henrik Renlund
 #' @export
 
-data_man_get_recode <- function(container, file = "", lab.prefix = "tab:recode_", ...){
+data_man_get_recode <- function(container, file = "", lab.prefix = "tab:recode_", where = "htb", clearpage = NULL, ...){
    if(missing(container)){
       tryCatch(expr = container <- get("data_man_container", envir = .GlobalEnv),
                error = function(e) stop("[data_man_get_recode] no default container available"))
    }
    ttify <- function(s) if(length(s)) gsub("_", "\\_", paste0("\\texttt{", s, "}"), fixed=TRUE ) else ""
+   dummy <- 0
+   if(!is.null(clearpage)) clearpage <- cumsum(rep(clearpage, length(container)))
    for(k in seq_along(container)){
       X <- container[[k]]
-
       if(is.null(recode <- X$recode)){
          next
       } else {
@@ -36,6 +39,22 @@ data_man_get_recode <- function(container, file = "", lab.prefix = "tab:recode_"
                    caption = paste0("Recoding of data base ",ttify(X$where), " entry ",ttify(X$var)," into ",ttify(X$name),"."),
                    label = paste0(lab.prefix, X$name),
                    ...)
+      dummy <- dummy + 1
+      if(dummy %in% clearpage) cat("\n\\clearpage\n")
    }
    invisible(NULL)
+}
+
+#' @title Recoded variables
+#' @description Get the names of the variables that have been recoded
+#' @param container the 'data_man' container
+#' @author Henrik Renlund
+#' @export
+
+data_man_recoded <- function(container){
+    if(missing(container)){
+        tryCatch(expr = container <- get("data_man_container", envir = .GlobalEnv),
+                 error = function(e) stop("[data_man_recoded] no default container available"))
+    }
+    names(container)[unlist(lapply(container, function(x) !is.null(x$recode)))]
 }
